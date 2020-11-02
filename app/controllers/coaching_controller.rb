00f1259a53bad8f7be5d40385c9e6310723d2b28
margarida santos <margarida.mimosa99@gmail.com>
2020-11-02 12:06:43 +0000

    coaching_controller

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,21 @@
+class CoachingController < ApplicationController
+    def answer
+        @question = params[:question]
+        @answer = coach_answer(@question)
+    end
+
+    def ask
+    end
+
+    private
+
+    def coach_answer
+        if question.downcase == "i am going to work right now!"
+            ''
+        elsif question.end_with?("?")
+            "Silly question, get dressed and go to work!"
+        else
+            "I don't care, get dressed and go to work!"
+        end
+    end
+end
